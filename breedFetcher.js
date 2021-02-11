e95b909cf0794f10d3893179a6e1dbbd6698fcb0
Brody Bayley <brodybayley@gmail.com>
2021-02-11 21:20:36 +0000

    added if else statements for breed fetcher and finished code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,3 +1,16 @@
 const request = require('request');
+const breed = process.argv.slice(2);
 
-fetch(https://api.thecatapi.com/v1/breeds/search?q=sib);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  }
+  const data = JSON.parse(body);
+  if (!data[0]) {
+    console.log('breed not found');
+  } else {
+    console.log(data);
+    console.log(typeof data);
+    console.log(data[0].description);
+  }
+});
