4170a3b4618977731ac2abb4a0dc30398fe81c75
Brody Bayley <brodybayley@gmail.com>
2021-02-11 22:10:40 +0000

    made a modification to fetchBreedDescription function and corrected return statement

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,22 @@
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`);
+// const breedName = process.argv[2];
 
 const fetchBreedDescription = function (breedName, callback) {
-
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      console.log('breed not found');
+    } else {
+      console.log(data);
+      console.log(typeof data);
+      console.log(data[0].description);
+    }
+  });
 };
 
 module.exports = { fetchBreedDescription };
-// request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-//   if (error) {
-//     console.log('error:', error);
-//   }
-//   const data = JSON.parse(body);
-//   if (!data[0]) {
-//     console.log('breed not found');
-//   } else {
-//     console.log(data);
-//     console.log(typeof data);
-//     console.log(data[0].description);
-//   }
-// });
 
