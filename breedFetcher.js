88f145aad3db2ce78d2f5dccbf77c3df7d0a56b4
Brody Bayley <brodybayley@gmail.com>
2021-02-11 22:55:25 +0000

    added test cases and modified code in breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,5 @@
 const request = require('request');
 
-// const breedName = process.argv[2];
 
 const fetchBreedDescription = function (breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
@@ -8,15 +7,20 @@ const fetchBreedDescription = function (breedName, callback) {
       return callback(error, null);
     }
     const data = JSON.parse(body);
+    console.log(data)
     if (!data[0]) {
-      console.log('breed not found');
+      return callback(error, 'breed not found');
     } else {
-      console.log(data);
-      console.log(typeof data);
-      console.log(data[0].description);
+      return callback(body, data[0].description);
     }
   });
 };
 
 module.exports = { fetchBreedDescription };
+
+
+
+// console.log(data);
+// console.log(typeof data);
+// console.log(data[0].description);
 
