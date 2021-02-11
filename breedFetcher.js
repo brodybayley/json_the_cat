e71049944fd1c73325435a0c23de61890094fa68
Brody Bayley <brodybayley@gmail.com>
2021-02-11 21:45:34 +0000

    adding index and moving function to index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,23 @@
 const request = require('request');
-const breed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  }
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log('breed not found');
-  } else {
-    console.log(data);
-    console.log(typeof data);
-    console.log(data[0].description);
-  }
-});
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`);
+
+const fetchBreedDescription = function (breedName, callback) {
+
+};
+
+module.exports = { fetchBreedDescription };
+// request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+//   if (error) {
+//     console.log('error:', error);
+//   }
+//   const data = JSON.parse(body);
+//   if (!data[0]) {
+//     console.log('breed not found');
+//   } else {
+//     console.log(data);
+//     console.log(typeof data);
+//     console.log(data[0].description);
+//   }
+// });
+
